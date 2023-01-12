9af25beb60f2a14d532210704e8f64868c11737a
yamil <yoms.tf@gmail.com>
2023-01-12 03:27:25 +0000

    Implementing the navbar and the search input group in the component Home

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
@@ -9,19 +7,66 @@ const Home = () => {
 		console.log('HEYYY')
 	}
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success" onClick={()=> prueba()}>
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+
+			{/*THIS IS THE PSEUDO NAVBAR*/}
+			<nav className="navbar bg-body-tertiary">
+  				<div className="container-fluid">
+    				<a className="navbar-brand d-flex my-auto" href="#">
+						<span className="material-icons mt-1 mx-2">
+							star
+						</span>
+						<div>Estudiar - Mexico City</div>
+					</a>
+					<a href="#">Back to Edvisor for Agents</a>
+  				</div>
+			</nav>
+
+			{/*THIS IS SEARCH INPUTS GROUP*/}
+			<div className="container">
+				<div className="row">
+					<div className="col form-floating">
+						<select className="form-select form-select-lg h-100" aria-label=".form-select-lg example">
+							<option defaultValue >Country</option>
+							<option value="1">One</option>
+							<option value="2">Two</option>
+							<option value="3">Three</option>
+						</select>
+						<label className="mx-3" htmlFor="floatingSelect">Destination</label>
+					</div>
+					<div className="col form-floating">
+						<select className="form-select form-select-lg h-100" aria-label=".form-select-lg example">
+							<option defaultValue >Provider</option>
+							<option value="1">One</option>
+							<option value="2">Two</option>
+							<option value="3">Three</option>
+						</select>
+						<label className="mx-3" htmlFor="floatingSelect">Provider</label>
+					</div>
+					<div className="col form-floating">
+						<select className="form-select form-select-lg h-100" aria-label=".form-select-lg example">
+							<option defaultValue >Weeks</option>
+							<option value="1">One</option>
+							<option value="2">Two</option>
+							<option value="3">Three</option>
+						</select>
+						<label className="mx-3" htmlFor="floatingSelect">Min. number of weeks</label>
+					</div>
+					<div className="col align-self-center">
+						<button type="button" className="btn btn-primary my-auto" onClick={prueba}>
+						<div className="d-flex">
+							<span className="material-icons">
+								search
+							</span>
+							<div>
+								Search
+							</div>
+						</div>
+						</button>
+					</div>
+				</div>
+			</div>
+
 		</div>
 	);
 };
