a9d9f221952d4001d013e9eecfca182288f2de2e
yamil <yoms.tf@gmail.com>
2023-01-19 02:50:12 +0000

    Adding hooks

--- src/js/component/Country.jsx
+++ src/js/component/Country.jsx
@@ -1,29 +1,29 @@
 import React, { useState, useEffect} from "react";
 
 export const Country = (props) => {
-    const [countryCampus, setCountryCampus] = useState([]);
-    const dataCampus = props.country
+    const dataCampus = props.country;
+    const [test, setTest]= useState([])
+    const obj = {};
 
-    useEffect(()=>{
-		const getCountry = () => {
-            dataCampus.map(item => setCountryCampus([...countryCampus, item.name]))
-        }
-		getCountry();
-	},[])
-
-    //console.log(countryCampus)
-   
+    
     return (
         <div className="col form-floating">
 			<select className="form-select form-select-lg h-100" aria-label=".form-select-lg example" >
 			<option defaultValue key="0">Country</option>
         { dataCampus.map((item, idx)=>{
-            let x = item.name.split(",").slice(-1)
-            console.log(x)
+
+            const individualCountry = item.name.split(",").slice(-1)
+            const course = item.name.split(",").slice(0, -1)
+            obj[individualCountry] += course;
+            console.log(course)
+            
+
             return(
-                <option value={idx} key={idx+1}>{item.name.split()}</option>
+                <option value={idx} key={idx+1}>{individualCountry}</option>
             )
+
         })}
+
             </select>
             <label className="mx-3" htmlFor="floatingSelect">Destination</label>
 		</div>
