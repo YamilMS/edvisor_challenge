ca2fa7f4f0a7e69470e9302cd1f03f5511e2a577
yamil <yoms.tf@gmail.com>
2023-01-22 03:00:44 +0000

    Weeks input finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect} from "react";
 import { Country } from "./Country.jsx";
+import { Weeks } from "./Weeks.jsx";
 import flag from "../img/mexFlag.jpg"
 import file from "../../data2.json"
 
@@ -103,15 +104,7 @@ const Home = () => {
 											</select>
 											<label className="mx-3" htmlFor="floatingSelect">Provider</label>
 										</div>
-										<div className="col form-floating">
-											<select className="form-select form-select-lg h-100" aria-label=".form-select-lg example">
-												<option defaultValue >Weeks</option>
-												<option value="1">One</option>
-												<option value="2">Two</option>
-												<option value="3">Three</option>
-											</select>
-											<label className="mx-3" htmlFor="floatingSelect">Min. number of weeks</label>
-										</div>
+										<Weeks />
 										<div className="col align-self-center d-flex justify-content-center">
 											<button type="button" className="btn btn-primary my-auto d-flex">
 												<div className="d-flex justify-content-between w-100">
