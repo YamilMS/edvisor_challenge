b678ec2481ecf5b3b06caf1fd72f33b6acf27667
yamil <yoms.tf@gmail.com>
2023-01-26 16:16:01 +0000

    Scenario 1 completed, now it shows the country selected on blur or when press confirm

--- src/js/component/Country.jsx
+++ src/js/component/Country.jsx
@@ -2,34 +2,28 @@ import React, { useState, useEffect} from "react";
 
 export const Country = (props) => {
     const dataCampus = props.country;
-    const [country, setCountry] = useState([])
+    const [displayCountry, setDisplayCountry] = useState([])
     const [showCountry, setShowCountry] = useState(false)
+    const [country, setCountry] = useState([])
     console.log(country)
 
     
     const selectCountry = (e) =>{
-        if(!country.includes(e.target.id)){
-            setCountry([...country, e.target.id]);
+        if(!displayCountry.includes(e.target.id)){
+            setDisplayCountry([...displayCountry, e.target.id]);
             e.target.style.background = 'lightBlue';
             e.target.querySelector('span').style.display= 'block';
         } else {
             e.target.style.background = 'transparent';
             e.target.querySelector('span').style.display= 'none';
-            setCountry(country.filter((item, idx) => item !== e.target.id))
+            setDisplayCountry(displayCountry.filter((item, idx) => item !== e.target.id))
         }
-        /*
-        country.map((item, index)=>{
-            const elm = document.getElementById(item)
-            elm.style.background= 'lightBlue';
-            elm.querySelector('span').style.display= 'block';
-        })
-        */
     }
 
     const deleteCountry= (e)=>{
-		const newSelectCountry= country.filter((item, idx)=>idx !== parseInt(e.target.id));
-		setCountry(newSelectCountry);
-        country.map((item, index)=>{
+		const newSelectCountry= displayCountry.filter((item, idx)=>idx !== parseInt(e.target.id));
+		setDisplayCountry(newSelectCountry);
+        displayCountry.map((item, index)=>{
             const elm = document.getElementById(item)
             if(parseInt(e.target.id) === index){
                 elm.style.background= 'transparent';
@@ -51,7 +45,7 @@ export const Country = (props) => {
         )
     })
 
-    const countrySelected = country.map((item, idx) =>{
+    const countrySelected = displayCountry.map((item, idx) =>{
         return(
             <li className="list-group-item d-flex justify-content-between my-2 py-0 px-2 border-0" key={idx} id={idx} style={{background:"#dedede"}}>
                 {item}
@@ -64,7 +58,7 @@ export const Country = (props) => {
                                             /* INPUT BOX*/ 
         <div className="col">
                 <form className="form-floating d-flex align-items-center">
-                    <input type="email" className="form-control" id="floatingInputValue" defaultValue={"Search"} onChange={(e)=>setCountry([...country, e.target.value])} onClick={()=>!showCountry ? (setShowCountry(!showCountry)) : null}/>
+                    <input type="email" className="form-control" id="floatingInputValue" value={country.length>0 ? `${country.length} Selected` : "Search"} onChange={(e)=>setDisplayCountry([...country, e.target.value])} onClick={()=>!showCountry ? (setShowCountry(!showCountry)) : null}/>
                     <span className="material-icons" style={{position:"absolute", color:"grey",  left: "85%", top:"40%"}}>
 									search
 					</span>
@@ -73,7 +67,7 @@ export const Country = (props) => {
 
                                             {/*DISPLAY OF COUNTRIES*/}
             { showCountry ? (
-                <div className="w-50 my-1"  style={{position:"absolute", marginLeft:"0.8em"}} onMouseLeave={()=> showCountry ? (setShowCountry(!showCountry)) : null}>
+                <div className="w-50 my-1"  style={{position:"absolute", marginLeft:"0.8em"}} onMouseLeave={()=>{ showCountry ? (setShowCountry(!showCountry)) : null; setCountry(displayCountry)}}>
                 <div className="row d-flex shadow border" style={{background:"white"}}>
                                 <div className="scrollpsy col-8 p-0">
                                     <p className="fw-bolder small mx-3 my-2">Results</p>
@@ -82,7 +76,7 @@ export const Country = (props) => {
                                     </ul>
                                 </div>
                                 <div className="scrollpsy col-4 d-flex flex-column align-items-center justify-items-between p-0" style={{background:"#f6f6f6"}}>
-                                    <p className="d-flex align-self-start small fw-bolder mx-3 my-2">Selected({country.length})</p>
+                                    <p className="d-flex align-self-start small fw-bolder mx-3 my-2">Selected({displayCountry.length})</p>
                                     <ul className="list-group w-75">
                                        {countrySelected}
                                     </ul>
@@ -91,7 +85,7 @@ export const Country = (props) => {
                 <div className="row d-flex shadow border">
                     <div className="d-flex justify-content-end p-2" style={{background:"white"}}> 
                         <button type="button" className="btn">Cancel</button>
-                        <button type="button" className="confirmButton btn btn-outline-secondary" style={{color:"black"}}>Confirm</button>
+                        <button type="button" className="confirmButton btn btn-outline-secondary" onClick={()=> setCountry(displayCountry)} style={{color:"black"}}>Confirm</button>
                     </div>
                 </div>
             </div>
@@ -105,7 +99,7 @@ export const Country = (props) => {
                                     </ul>
                                 </div>
                                 <div className="col-4 d-flex flex-column align-items-center justify-items-between p-0" style={{background:"#f6f6f6"}}>
-                                    <p className="d-flex align-self-start small fw-bolder mx-3 my-2">Selected({country.length})</p>
+                                    <p className="d-flex align-self-start small fw-bolder mx-3 my-2">Selected({displayCountry.length})</p>
                                     <ul className="list-group w-75">
                                        {countrySelected}
                                     </ul>
